ade128aaadb5983d8b814d15064498e1e698bf68
Shiino Aya <26667561+dobby618@users.noreply.github.com>
2020-06-30 05:57:11 +0900

    basic 認証を追加する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :basic_authentication if Rails.env.production?
   before_action :authenticate_user!
+
+  private
+
+  def basic_authentication
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 end
